b6a3b65b1571eda3249a92d53137b0e2dbb94dcc
abhirammv <amadenur@ucsc.edu>
2017-07-24 08:50:55 -0700

    Linear Regression Implementation - Added documentation

--- LinearRegression/SimpleLinReg.py
+++ LinearRegression/SimpleLinReg.py
@@ -1,1 +1,16 @@
 #Implementation of Simple Linear Regression
+#Steps:
+"""
+1) Preprocess the data -> Can be done by reusing the functions in DPP.py
+    1. Handle missing values
+    2. Apply feature scaling if required **
+    3. Split the data into training and test sets
+
+2) Fit the linear regression model found in the sklearn package
+
+** -> Some functions take have built-in feature scaling. Read the documentation once to be thorough
+"""
+
+
+if __name__ == "__main__":
+    print('Hello')
