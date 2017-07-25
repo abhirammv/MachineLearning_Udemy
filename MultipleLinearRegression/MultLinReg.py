3672d70dba3d324e93415d2d213917ecef9fbac3
abhirammv <amadenur@ucsc.edu>
2017-07-24 23:44:10 -0700

    Initial Commit - Multiple linear regression

---
+++ MultipleLinearRegression/MultLinReg.py
@@ -0,0 +1,3 @@
+#Implementation of Multiple Linear Regression
+#The procedure is very similar to Simple linear regression, except that you have multiple predictors
+#Preprocess the data - Missing value handling, outlier handling, feature scaling and obtaining the train/test data split
