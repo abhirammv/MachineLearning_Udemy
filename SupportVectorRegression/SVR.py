138a8ba0c295dc54a20bf9a48c6e1085e1d93d58
abhirammv <amadenur@ucsc.edu>
2017-08-03 19:04:48 -0700

    SVR implementation

--- SupportVectorRegression/SVR.py
+++ SupportVectorRegression/SVR.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy
+import numpy as np
 
 def read_file(file_name):
     dataset = pd.read_csv("{0}.csv".format(file_name));
@@ -18,7 +18,7 @@ def get_InOutparams(data):
 
 def FeatureScaler(array):
     from sklearn.preprocessing import StandardScaler
-    SS = StandardScaler();
+    SS = StandardScaler()
     array = SS.fit_transform(array)
     return array
 
@@ -45,8 +45,11 @@ if __name__ == "__main__":
     dataset = read_file("Position_Salaries")
     X, y = get_InOutparams(dataset)
 
+    y_temp = y;
+
     #Ignoring the Position Parameter in this dataset
     X = X[:, 1:]
+    x_temp = X;
 
     #Need to scale data for SVR
     X = FeatureScaler(X)
@@ -57,6 +60,27 @@ if __name__ == "__main__":
     visualize(X, y, SVRegressor)
 
     #An example showing a prediction when there is scaling:
+    from sklearn.preprocessing import StandardScaler
+    SS_x = StandardScaler()
+    SS_y = StandardScaler()
+
+    SS_x.fit_transform(x_temp)
+    SS_y.fit_transform(y_temp)
+
+    x_scaled = SS_x.transform(np.array([[6.5]]))
+
+    y_pred = SVRegressor.predict(6.5)
+    y_pred_1 = SS_y.inverse_transform(y_pred)
+
+    y_pred_scaled = SVRegressor.predict(x_scaled)
+    y_pred_scaled_1 = SS_y.inverse_transform(y_pred_scaled)
+
+    print("Without Scaling 6.5, model says: {0}\n".format(y_pred))
+    print("Without Scaling 6.5, the inverse transformation on the model's output is: {0}\n".format(y_pred_1))
+
+    print("Scaling 6.5, model says: {0}\n".format(y_pred_scaled))
+    print("Scaling 6.5, the inverse transformation on the model's output is: {0}\n".format(y_pred_scaled_1))
+
 
 
 
