187f0774bbe7e2c0def06acbd7e6e2f931fd6b5b
abhirammv <amadenur@ucsc.edu>
2017-08-03 22:57:32 -0700

    log reg commit

--- LogisticRegression/LogReg.py
+++ LogisticRegression/LogReg.py
@@ -42,6 +42,27 @@ def GetConfMatrix(actual_values, pred_values):
     cm = confusion_matrix(actual_values, pred_values)
     return cm
 
+def visualize(in_data, out_data, model, mode):
+    from matplotlib.colors import ListedColormap
+    X_set, y_set = in_data, out_data
+    X1, X2 = np.meshgrid(np.arange(start=X_set[:, 0].min() - 1, stop=X_set[:, 0].max() + 1, step=0.01),
+                         np.arange(start=X_set[:, 1].min() - 1, stop=X_set[:, 1].max() + 1, step=0.01))
+    plt.contourf(X1, X2, model.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+                 alpha=0.75, cmap=ListedColormap(('red', 'green')))
+    plt.xlim(X1.min(), X1.max())
+    plt.ylim(X2.min(), X2.max())
+    for i, j in enumerate(np.unique(y_set)):
+        plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                    c=ListedColormap(('red', 'green'))(i), label=j)
+    if(mode=="training"):
+        plt.title('Logistic Regression (Training set)')
+    elif(mode=="test"):
+        plt.title('Logistic Regression (Test set)')
+    plt.xlabel('Age')
+    plt.ylabel('Estimated Salary')
+    plt.legend()
+    plt.show()
+
 if __name__ == "__main__":
     dataset = read_file("Social_Network_Ads")
     X, y = get_InOutparams(dataset)
@@ -53,51 +74,5 @@ if __name__ == "__main__":
     y_pred = Pred(X_te, LogRegClassifier)
     ConfMtx = GetConfMatrix(y_te, y_pred)
     print(ConfMtx)
-
-
-# Visualising the Training set results
-from matplotlib.colors import ListedColormap
-
-X_set, y_set = X_tr, y_tr
-
-X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
-                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
-
-
-plt.contourf(X1, X2, LogRegClassifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
-
-plt.xlim(X1.min(), X1.max())
-plt.ylim(X2.min(), X2.max())
-
-for i, j in enumerate(np.unique(y_set)):
-    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
-                c = ListedColormap(('red', 'green'))(i), label = j)
-
-plt.title('Logistic Regression (Training set)')
-plt.xlabel('Age')
-plt.ylabel('Estimated Salary')
-plt.legend()
-plt.show()
-
-# Visualising the Test set results
-
-X_set, y_set = X_te, y_te
-X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
-                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
-
-plt.contourf(X1, X2, LogRegClassifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
-
-plt.xlim(X1.min(), X1.max())
-plt.ylim(X2.min(), X2.max())
-
-for i, j in enumerate(np.unique(y_set)):
-    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
-                c = ListedColormap(('red', 'green'))(i), label = j)
-
-plt.title('Logistic Regression (Test set)')
-plt.xlabel('Age')
-plt.ylabel('Estimated Salary')
-plt.legend()
-plt.show()
+    visualize(X_tr, y_tr, LogRegClassifier, "training")
+    visualize(X_te, y_te, LogRegClassifier, "test")
