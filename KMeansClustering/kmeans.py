e994ebc671c940ae5ffaea5126635c2a5af12435
abhirammv <amadenur@ucsc.edu>
2017-08-18 17:12:13 -0700

    elbow method init

--- KMeansClustering/kmeans.py
+++ KMeansClustering/kmeans.py
@@ -10,6 +10,16 @@ def getParams(data):
     X = data.iloc[:,:].values
     return X
 
+def ElbowMethod(array):
+    #This method helps in determining the optimum K value in K Means Clustering algorithm
+    from sklearn.cluster import KMeans
+    wcss = []
+    for i in range(1, 11):
+        kmeans = KMeans(n_clusters=i, init="k-means++", max_iter=300, n_init=10)
+        kmeans.fit(array)
+        wcss.append(kmeans.inertia_)
+    #Plotting
+
 
 
 
@@ -20,6 +30,7 @@ if __name__ == "__main__":
     X = getParams(data)
     #extracting the right amount of data
     X = X[:, [3,4]]
+
 
 
 
