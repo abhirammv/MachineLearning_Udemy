179710fe6557c0c94f60ccb9ddbf7e327c774a5b
abhirammv <amadenur@ucsc.edu>
2017-08-18 23:34:41 -0700

    Assoc. learning init

--- AssociationRuleApriori/apriori.py
+++ AssociationRuleApriori/apriori.py
@@ -1,3 +1,17 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+
+def read_file(file_name):
+    dataset = pd.read_csv("{0}.csv".format(file_name));
+    return dataset
+
+def getParams(data):
+    X = data.iloc[:, :].values
+    return X
+
+
+
+if __name__ == "__main__":
+    data = read_file("Market_Basket_Optimisation")
+
