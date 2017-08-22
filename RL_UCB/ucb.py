837289bce52cd7b8e5e06a6d0148557e3f42ec92
abhirammv <amadenur@ucsc.edu>
2017-08-22 00:14:19 -0700

    edit

--- RL_UCB/ucb.py
+++ RL_UCB/ucb.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 
 def read_file(file_name):
     dataset = pd.read_csv("{0}.csv".format(file_name), header=None);
@@ -15,5 +16,22 @@ if __name__=="__main__":
 
     num_times_selected = [0] * d
     sum_rewards = [0] * d
+
+    for n in range(0, N):
+        max_upper_bound = 0
+        for i in range(0, d):
+            if(num_times_selected[i] > 0):
+                print("do something")
+            else:
+                upper_bound = 1e400
+            if(upper_bound > max_upper_bound):
+                max_upper_bound = upper_bound
+                ad = i
+
+        num_times_selected[ad] = num_times_selected[ad] + 1
+        reward = dataset.values[n, ad]
+        sum_rewards[ad] = sum_rewards[ad] + reward
+
+
 
 
