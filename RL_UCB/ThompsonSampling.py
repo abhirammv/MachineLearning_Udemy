ed54a4fef257b18f221538c4cfea31a6ef901d2e
abhirammv <amadenur@ucsc.edu>
2017-08-23 18:54:29 -0700

    Thompson Sampling

--- RL_UCB/ThompsonSampling.py
+++ RL_UCB/ThompsonSampling.py
@@ -22,4 +22,21 @@ if __name__ == "__main__":
         max_random = 0
         for i in range(0, d):
             random_beta = random.betavariate(numbers_of_rewards_1[i] + 1, numbers_of_rewards_0[i] + 1)
+            if random_beta > max_random:
+                max_random = random_beta
+                ad = i
+
+        ads_selected.append(ad)
+        reward = dataset.values[n, ad]
+        if reward == 1:
+            numbers_of_rewards_1[ad] = numbers_of_rewards_1[ad] + 1
+        else:
+            numbers_of_rewards_0[ad] = numbers_of_rewards_0[ad] + 1
+
+    plt.hist(ads_selected)
+    plt.title('Histogram of ads selections')
+    plt.xlabel('Ads')
+    plt.ylabel('Number of times each ad was selected')
+    plt.show()
+
 
